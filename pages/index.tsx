b97b60bf934bea1fec9c90272c7433945cf3ffc1
okabasele <ocekabasele@gmail.com>
2022-08-26 23:56:22 +0200

    refacto

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
+
 import { useSupabase } from "../utils/useSupabase";
+import { useProducts } from "../utils/useProducts";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 
@@ -38,96 +40,18 @@ export type TypeClothes = {
 
 export default function Home() {
   const supabaseClient = useSupabase();
+  const {
+    products,
+    size,
+    colors,
+    typeClothes,
+    categories
+  } = useProducts()
 
-  const [products, setProducts] = useState<Product[]>([]);
-  const [size, setSize] = useState<Size[]>([]);
-  const [colors, setColors] = useState<Color[]>([]);
-  const [categories, setCategories] = useState<Category[]>([]);
-  const [typeClothes, setTypeClothes] = useState<TypeClothes[]>([]);
   const carousel = [
     "https://images.unsplash.com/photo-1603252109360-909baaf261c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80",
     "https://images.unsplash.com/photo-1489987707025-afc232f7ea0f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80",
   ];
-  useEffect(() => {
-    async function readProducts() {
-      // Reading
-      const { data, error } = await supabaseClient.from("Product").select("*");
-
-      if (data) {
-        const productsToSave = data.map((element) => {
-          const typeProd = typeClothes.find((e) => e.id === element.typeID);
-          
-          const colorProd = colors.find((e) => e.id === element.colorID);
-          const categoryProd = categories.find(
-            (e) => e.id === typeProd?.categoryID
-          );
-          
-          return {
-            id: element.id,
-            title: element.title,
-            description: element.description,
-            price: element.price,
-            image: element.image,
-            type: typeProd?.name || "",
-            category: categoryProd?.name || "",
-            color: colorProd?.name || "",
-          }
-        });
-        
-        setProducts([...productsToSave]);
-
-        localStorage.setItem("products", JSON.stringify(productsToSave));
-        
-        if (!localStorage.getItem("cart")) {
-          localStorage.setItem("cart", "");
-        }
-      }
-    }
-
-    async function readSize() {
-      // Reading
-      let { data, error } = await supabaseClient
-        .from("Size")
-        .select("id, name");
-      if (data) {
-        setSize(data);
-      }
-    }
-    async function readColors() {
-      // Reading
-      let { data, error } = await supabaseClient
-        .from("Color")
-        .select("id, name");
-      if (data) {
-        setColors(data);
-      }
-    }
-    async function readTypeClothes() {
-      // Reading
-      let { data, error } = await supabaseClient
-        .from("Type")
-        .select("id, name,categoryID");
-      if (data) {
-        setTypeClothes(data);
-      }
-    }
-
-    async function readCategories() {
-      // Reading
-      let { data, error } = await supabaseClient
-        .from("Category")
-        .select("id, name");
-      if (data) {
-        setCategories(data);
-      }
-    }
-
-    readColors();
-    readSize();
-    readTypeClothes();
-    readCategories();
-    readProducts();
-  }, []);
 
   return (
     <>
